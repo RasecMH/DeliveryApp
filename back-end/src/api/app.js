e0bb087121eca501c083b1e86d2b5256aceeaece
Thariklz <thariklz19@gmail.com>
2022-11-30 15:34:53 -0300

    fix[Model Sales]: error association

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -6,8 +6,7 @@ const app = express();
 app.use(express.json());
 
 app.get('/coffee', (_req, res) => res.status(418).end());
-app.use(loginRouter);
-
+app.use('/users', loginRouter);
 
 app.use(handleError);
 
