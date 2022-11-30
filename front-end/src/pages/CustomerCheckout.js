faa4f124f8dc1039f5eec76bc018596e417d5757
RasecMH <rasecmh0@gmail.com>
2022-11-30 16:35:58 -0300

    feat: router and pages setup

---
+++ front-end/src/pages/CustomerCheckout.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function CustomerCheckout() {
+  return (
+    <div>
+      <h1>Hello do CustomerCheckout</h1>
+    </div>
+  );
+}
+
+export default CustomerCheckout;
---
+++ front-end/src/pages/CustomerOrders.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function CustomerOrders() {
+  return (
+    <div>
+      <h1>Hello do CustomerOrders</h1>
+    </div>
+  );
+}
+
+export default CustomerOrders;
