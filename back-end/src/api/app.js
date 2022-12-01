cbea3e7632ee7b88f7b944579bfacfeebdd66cc4
Jessy Damasceno <jessy_bas@hotmail.com>
2022-12-01 00:11:28 -0300

    feat: app.use('/sales')

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const loginRouter = require('./routes/loginRouter');
 const handleError = require('./middlewares/handleError');
 const productRouter = require('./routes/productRouter');
+const salesRouter = require('./routes/salesRouter');
 
 const app = express();
 app.use(express.json());
@@ -9,6 +10,7 @@ app.use(express.json());
 app.get('/coffee', (_req, res) => res.status(418).end());
 app.use('/users', loginRouter);
 app.use('/products', productRouter);
+app.use('/sales', salesRouter);
 
 app.use(handleError);
 
