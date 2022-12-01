e5457eb8bb52ec8b26392d22f4bd38a37afe75dd
RasecMH <rasecmh0@gmail.com>
2022-12-01 20:15:13 -0300

    add[OrderDetails]: html with data-testids

---
+++ front-end/src/components/OrderDetailsHeader.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function OrderDetailsHeader() {
+  return (
+    <div>
+      <span>PEDIDO 0003</span>
+      <span>P. Venda: Fulanda Pereira</span>
+      <span>07/04/2021</span>
+      <span>Entregue</span>
+      <button type="button">MARCAR COMO ENTREGUE</button>
+    </div>
+  );
+}
+
+export default OrderDetailsHeader;
---
+++ front-end/src/components/OrderDetailsTotalDisplay.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function OrderDetailsTotalDisplay() {
+  return (
+    <div>
+      <span>Total: R$ 28,46</span>
+    </div>
+  );
+}
+
+export default OrderDetailsTotalDisplay;
