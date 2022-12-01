f13c6017d9d5daa69cd89356d2d399f39828f55a
Jessy Damasceno <jessy_bas@hotmail.com>
2022-12-01 00:45:11 -0300

    feat[SaleController]: implements getAll and getById request methods

---
+++ back-end/src/api/controllers/SaleController.js
@@ -0,0 +1,30 @@
+const SaleService = require('../services/SaleService');
+
+class ProductController {
+  constructor() {
+    this.service = new SaleService();
+  }
+
+   async getAll(_req, res, next) {
+    try {
+      const result = await this.service.getAll();
+
+      return res.status(200).json(result);
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async getById(req, res, next) {
+    try {
+      const { id } = req.params;
+      const result = await this.service.getById(id);
+
+      return res.status(200).json(result);
+    } catch (error) {
+      next(error);
+    }
+  }
+}
+
+module.exports = ProductController;
