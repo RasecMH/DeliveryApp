2937508ef768c638cb8e2117a7a0ce7a07d76e17
RasecMH <rasecmh0@gmail.com>
2022-12-06 07:25:01 -0300

    feat[SellerOrders]: api request

--- front-end/src/components/SellerOrderDetailsHeader.js
+++ front-end/src/components/SellerOrderDetailsHeader.js
@@ -1,37 +1,67 @@
 import React from 'react';
+import { PropTypes } from 'prop-types';
+// import { useHistory } from 'react-router';
+import axios from 'axios';
 
-function SellerOrderDetailsHeader() {
+function SellerOrderDetailsHeader({ idPedido, saleDate, saleStatus }) {
+  // const history = useHistory();
+
+  const attStatus = async (stat) => {
+    try {
+      await axios.put(
+        'http://localhost:3001/sales/status/att',
+        { id: idPedido, status: stat },
+      );
+      // history.go(0);
+    } catch (error) {
+      console.log(error.response.data);
+    }
+  };
+
   return (
     <div>
       <span
         data-testid="seller_order_details__element-order-details-label-order-id"
       >
-        PEDIDO 0001
+        PEDIDO 000
+        {idPedido}
       </span>
       <span
         data-testid="seller_order_details__element-order-details-label-order-date"
       >
-        08/04/2021
+        {saleDate.toLocaleDateString()}
       </span>
       <span
         data-testid="seller_order_details__element-order-details-label-delivery-status"
       >
-        Pendente
+        {saleStatus}
       </span>
       <button
+        onClick={ () => attStatus('Preparando') }
         data-testid="seller_order_details__button-preparing-check"
         type="button"
+        disabled={ saleStatus !== 'Pendente' }
+
       >
-        MARCAR COMO ENTREGUE
+        PREPARAR PEDIDO
       </button>
       <button
+        onClick={ () => attStatus('Em Trânsito') }
         data-testid="seller_order_details__button-dispatch-check"
         type="button"
+        disabled={ saleStatus !== 'Preparando' }
       >
         SAIU PARA ENTREGA
       </button>
     </div>
   );
 }
+
+SellerOrderDetailsHeader.propTypes = {
+  idPedido: PropTypes.number,
+  sellerName: PropTypes.string,
+  saleDate: PropTypes.string,
+  saleStatus: PropTypes.string,
+}.isRequired;
 
 export default SellerOrderDetailsHeader;
